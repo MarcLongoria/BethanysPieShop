b35615a355c3fe22af49bf9a3ba5cc373b701951
Marc Longoria <marclongoria@outlook.com>
2017-06-27 00:46:13 -0500

    Adding EF and connection string

---
+++ BethanysPieShop/BethanysPieShop/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(LocalDB)\\mssqllocaldb;AttachDbFilename=BethanysPieShop;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
